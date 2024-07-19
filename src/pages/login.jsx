2b952a68ce3aaf30e91c8b4926609dd3def7f1cb
collabsia <collabsia4@gmail.com>
2024-07-19 20:06:55 +0800

    Update login.jsx

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -12,7 +12,7 @@ const Login = ({ history }) => {
     try {
       const token = response.credential;
     
-      const { data } = await axios.post('/api/login', {
+      const { data } = await axios.post('https://server-gzmw.onrender.com/api/login', {
         token: token,
       });
 
